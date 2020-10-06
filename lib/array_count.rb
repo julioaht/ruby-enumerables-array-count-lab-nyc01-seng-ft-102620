6eff204009784374d958dc13d2ad6c29528e7ee6
julio hernandez <julioandres72@gmail.com>
2020-10-06 17:40:26 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -9,5 +9,6 @@ end
 def count_empty_strings(array)
   array.count do |empty_strings|
     new_array == ""
+  end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
