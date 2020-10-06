91ac24ad6191cfba6dc78b63dc2a303e54638b3f
julio hernandez <julioandres72@gmail.com>
2020-10-06 14:32:33 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,13 @@
+require "pry"
+
 def count_strings(array)
+  array.count do |only_str|
+    only_str.count
+  end
   # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
+
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
