7a6bad990a2dcbcf923dad425c4ea7c9cbdbf064
julio hernandez <julioandres72@gmail.com>
2020-10-06 17:26:20 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
   array.count do |new_array|
-    new_array.class == strings
+    new_array.class(strings) 
   end
   # Return the total number of strings in the provided array using the count enumerable
 end
